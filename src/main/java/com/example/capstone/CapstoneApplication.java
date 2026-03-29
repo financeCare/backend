116a3c0ad15d6a423b0f80c73ef329ce0fe3e857
c3b22 <Soranut169@gmail.com>
2026-03-30 00:42:36 +0700

    feat: initialize Spring Boot application with security, database, and external service configurations

--- src/main/java/com/example/capstone/CapstoneApplication.java
+++ src/main/java/com/example/capstone/CapstoneApplication.java
@@ -1,14 +1,22 @@
 package com.example.capstone;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class CapstoneApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Bangkok"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneApplication.class, args);
